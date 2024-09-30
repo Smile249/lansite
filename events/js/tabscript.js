95dcad8acb36b8dbd8990334534ec95d554a71b9
Smile <85846415+Smile249@users.noreply.github.com>
2024-09-30 20:24:06 +0200

    resolve conflict 2

--- events/js/tabscript.js
+++ events/js/tabscript.js
@@ -1,15 +1,5 @@
-function sanitize(input) {
-    var element = document.createElement('div');
-    element.appendChild(document.createTextNode(input));
-    return element.innerHTML;
-}
-
 function openTab(evt, tabName) {
     var i, tabcontent, tablinks;
-
-    // Sanitize tabName to avoid potential XSS
-    tabName = sanitize(tabName);
-
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
@@ -22,19 +12,18 @@ function openTab(evt, tabName) {
     evt.currentTarget.className += " active";
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-    var coll = document.getElementsByClassName("collapsible");
+//collapsibles
+var coll = document.getElementsByClassName("collapsible");
+var i;
 
-    for (let i = 0; i < coll.length; i++) {
-        coll[i].addEventListener("click", function() {
-            this.classList.toggle("open");
-
-            var content = this.nextElementSibling;
-            if (content && content.style.maxHeight) {
-                content.style.maxHeight = null; // Collapse
-            } else if (content) {
-                content.style.maxHeight = content.scrollHeight + "px"; // Expand
-            }
-        });
-    }
-});
+for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+        this.classList.toggle("open");
+        var content = this.nextElementSibling;
+        if (content.style.maxHeight){
+            content.style.maxHeight = null;
+        } else {
+            content.style.maxHeight = content.scrollHeight + "px";
+        }
+    });
+}
